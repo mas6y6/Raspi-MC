166a41cae1e6bf4119a6a1dcda574b48db7b216f
mas6y6pro <125225333+mas6y6pro@users.noreply.github.com>
2023-04-16 06:01:12 +0000

    update

--- packages/gui/main_gui.py
+++ packages/gui/main_gui.py
@@ -2,19 +2,18 @@ import os
 from getkey import getkey, keys
 
 def main_menu():
-    while True:
-        key = getkey()
-        os.system("clear")
-        print("========== Main menu ==========")
-        print()
-        print("1. Start Server")
-        print("2. Server Settings")
-        print("3. Plugins")
-        print("4. Worlds")
-        print("5. Exit")
-        print()
-        print("Input the number of the item")
-        print("===============================")
-        output = key
-        break
-    return output
+    key = getkey()
+    os.system("clear")
+    print("========== Main menu ==========")
+    print()
+    print("1. Start Server")
+    print("2. Server Settings")
+    print("3. Plugins")
+    print("4. Worlds")
+    print("5. Exit")
+    print()
+    print("Input the number of the item")
+    print("===============================")
+    return key
+
+main_menu()
