3d75536cf91f985a6885347022ba2e94a0a6b8a4
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 15:05:15 +0000

    update on files

--- Raspi-MC.py
+++ Raspi-MC.py
@@ -2,57 +2,79 @@ from subprocess import *
 import os
 import time
 from getkey import getkey, keys
-print("Loading GUI setup")
-
-#run("wget https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar",shell=True,stdout=None)
-
 cwd = os.getcwd()
 
-os.system("clear")
-print("=====================================")
-print()
-print("Welcome to the Raspi-MC (spigot mc)")
-print()
-print("A minecraft server installer")
-print()
-print("Version 1.0 / MC Version 1.19.4")
-print()
-print("This will install spigot mc server And prevent all the hassle of making it function")
-print("For more info click here: https://www.spigotmc.org/")
-print()
-print('Press "enter" to continue')
-print()
-print("=====================================")
-input()
-os.system("clear")
+found = False
+dirs = os.listdir()
+for i in range(len(dirs)):
+    s = i - 1
+    if dirs[s] == "packages":
+        found = True
+        break
 
-os.system("clear")
-print("=====================================")
-print()
-print("You need to have github and java 17 installed to install spigot")
-print()
-print('Press "enter" to continue')
-print()
-print("=====================================")
-input()
-os.system("clear")
+def setup():
+    print("Loading GUI setup")
+    os.system("clear")
+    print("=====================================")
+    print()
+    print("Welcome to the Raspi-MC (spigot mc)")
+    print()
+    print("A minecraft server installer")
+    print()
+    print("Version 1.0 / MC Version 1.19.4")
+    print()
+    print("This will install spigot mc server And prevent all the hassle of making it function")
+    print("For more info click here: https://www.spigotmc.org/")
+    print()
+    print('Press "enter" to continue')
+    print()
+    print("=====================================")
+    input()
+    os.system("clear")
 
-os.system("clear")
-print("=====================================")
-print()
-print("Downloading GUI menu from github...")
-print()
-print("=====================================")
+    os.system("clear")
+    print("=====================================")
+    print()
+    print("You need to have github and java 17 installed to install spigot")
+    print()
+    print('Press "enter" to continue')
+    print()
+    print("=====================================")
+    input()
+    os.system("clear")
+    print("=====================================")
+    print()
+    print("Downloading GUI menu from github...")
+    print()
+    print("=====================================")
+    run("git clone https://github.com/mas6y6pro/Raspi-MC.git --branch files --single-branch",shell=True)
+    run("cp -r ./Raspi-MC/packages ./",shell=True)
+    run("rm -rf Raspi-MC",shell=True)
 
-os.mkdir("gui")
+    time.sleep(2)
 
-os.chdir("gui")
+    os.system("clear")
 
-#Downloads GUI script
+    os.chdir(cwd)
+    print("=====================================")
+    print()
+    print("Downloading Spigot MC 1.19.4 ...")
+    print()
+    print("=====================================")
 
-os.system("")
+    #run("wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar",shell=True)
 
-os.chdir(cwd)
+    os.system("clear")
 
-time.sleep(2)
-os.system("clear")
+    os.chdir(cwd)
+    print("=====================================")
+    print()
+    print("Running Spigot...")
+    print()
+    print("=====================================")
+
+    #run("java -Xmx1042M -Xms1042M -jar BuildTools.jar nogui",shell=True)
+
+if found == False:
+    setup()
+
