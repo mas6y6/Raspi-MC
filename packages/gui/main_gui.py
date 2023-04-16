9550063038ab84c5e2f227344da24cae1c0ee43b
mas6y6pro <125225333+mas6y6pro@users.noreply.github.com>
2023-04-16 08:52:31 -0500

    Update main_gui.py

--- packages/gui/main_gui.py
+++ packages/gui/main_gui.py
@@ -1,20 +1,23 @@
-import os
-from getkey import getkey, keys
-
 def main_menu():
-    os.system("clear")
-    print("========== Main menu ==========")
-    print()
-    print("1. Start Server")
-    print("2. Server Settings")
-    print("3. Plugins")
-    print("4. Worlds")
-    print("5. Exit")
-    print()
-    print("Input the number of the item")
-    print("===============================")
-    key = getkey()
-    return key
-
-def alert():
-    pass
+    while True:
+        os.system("clear")
+        print("============== Main Menu ==============")
+        print()
+        print("1. Start Server")
+        print("2. Server Settings")
+        print("3. World Settings")
+        print("4. Plugins")
+        print("5. Logs")
+        print("6. Crash Reports")
+        print("7. More info")
+        print("8. Exit")
+        print()
+        print("=======================================")
+        key = getkey()
+        if key == "1" or key == "2" or key == "3" or key == "4" or key == "5" or key == "6" or key == "7" or key == "8":
+            output = key
+            break
+        else:
+            output = None
+    return output
+    os.system()
