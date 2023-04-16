82231f76e95798d2781b561778ae63786d91e360
mas6y6pro <125225333+mas6y6pro@users.noreply.github.com>
2023-04-16 08:51:54 -0500

    Create notifications.py

---
+++ packages/gui/notifications.py
@@ -0,0 +1,21 @@
+import os
+def Error(Error, Info):
+    os.system("clear")
+    print("============ Error =============")
+    print()
+    print(Error)
+    print()
+    print(Info)
+    print()
+    print('Press "enter" to close')
+    print("==============================")
+    input()
+    os.system("clear")
+
+def loading(info):
+    os.system("clear")
+    print("==============================")
+    print()
+    print(info)
+    print()
+    print("==============================")
